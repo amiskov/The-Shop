5a1861d9233be9f6c4f7bf3e0e76fce995b8430e
YmAngie <a.ymydykova@gmail.com>
2017-10-25 13:53:08 +0300

    Add EJS. Change media.less filename.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const browserSync = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const ejs = require('gulp-ejs');
+const gutil = require('gulp-util');
 
 // Автоперезагрузка при изменении файлов в папке `dist`:
 // Принцип: меняем файлы в `/src`, они обрабатываются и переносятся в `dist` и срабатывает автоперезагрузка.
@@ -37,14 +40,17 @@ gulp.task('js', () => {
 });
 
 gulp.task('html', () => {
-    gulp.src('src/index.html')
-        .pipe(gulp.dest('./dist'));
+    gulp.src('src/index.ejs')
+        .pipe(ejs().on('error', gutil.log))
+        .pipe(rename('index.html'))
+            .pipe(gulp.dest('./dist'));
 });
 
 // Отслеживание изменений в файлах, нужно только при локальной разработке
 gulp.task('watch', () => {
     gulp.watch('src/less/**/*.less', ['styles']);
     gulp.watch('src/**/*.html', ['html']);
+    gulp.watch('src/**/*.ejs', ['html']);
     gulp.watch('src/img/**/*.*', ['img']);
     gulp.watch('src/js/**/*.*', ['js']);
 });

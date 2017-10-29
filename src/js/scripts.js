5ab0db9b5c43c191ddd1a0b45ff3d1c997b1b577
YmAngie <a.ymydykova@gmail.com>
2017-10-29 16:54:34 +0300

    Use addEventListener for image switching, size and color choosing.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,74 +1,42 @@
-/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "[iI]gnored" }]*/
-
-function showTShirtIgnored (color) {
-    if (color === 'white') {
-        document.getElementById('tShirt').src='/img/tshirts/tshirt_white.jpg';
-        makeRedBorder('whiteBox');
-        returnBlackBorder('whiteBox');
-        
-    } else if (color === 'yellow') {
-        document.getElementById('tShirt').src='/img/tshirts/tshirt_yellow.jpg';
-        makeRedBorder('yellowBox');
-        returnBlackBorder('yellowBox');
-        
-    } else if (color === 'green') {
-        document.getElementById('tShirt').src='/img/tshirts/tshirt_green.jpg';
-        makeRedBorder('greenBox');
-        returnBlackBorder('greenBox');
+document.querySelector('.product-form').addEventListener('click', event => {
+    if (!event.target.matches('.js-product-details')) {
+        return;
     }
+
+    const target = event.target;
+
+    const color = target.getAttribute('data-value');
+    if (target.classList.contains('product-color__item')) {
+        document.getElementById('tShirt').src = '/img/tshirts/tshirt_' + color + '.jpg';
+    }
+
+    const colorElements = document.getElementsByClassName('product-color__item');
+    if (target.classList.contains('product-color__item')) {
+        updateBorders(colorElements, target);
+    }
+
+    const sizeElements = document.getElementsByClassName('product-size__item');
+    if (target.classList.contains('product-size__item')) {
+        updateBorders(sizeElements, target);
+    }
+});
+
+function updateBorders(elements, target) {
+    [].forEach.call(elements, function (element) {
+        if (element === target) {
+            activate(element, true);
+        } else {
+            activate(element, false);
+        }
+    });
 }
 
-function makeRedBorder (box) {
-    var redBorder = '2px solid #c40c00';
-    if (box === 'whiteBox') {
-        document.getElementById('whiteBox').style.border = redBorder;
-    } else if (box === 'yellowBox') {
-        document.getElementById('yellowBox').style.border = redBorder;
-    } else if (box === 'greenBox') { 
-        document.getElementById('greenBox').style.border = redBorder;
-    } else if (box === 'sBox') { 
-        document.getElementById('sBox').style.border = redBorder;
-    } else if (box === 'mBox') { 
-        document.getElementById('mBox').style.border = redBorder;
-    } else if (box === 'lBox') { 
-        document.getElementById('lBox').style.border = redBorder;
-    }
-}
-
-function returnBlackBorder (box) {
-    const blackBorder = '1px solid black';
-    if (box === 'whiteBox') {
-        document.getElementById('yellowBox').style.border = blackBorder;
-        document.getElementById('greenBox').style.border = blackBorder;
-    } else if (box === 'yellowBox') {
-        document.getElementById('whiteBox').style.border = blackBorder;
-        document.getElementById('greenBox').style.border = blackBorder;
-    } else if (box === 'greenBox') { 
-        document.getElementById('whiteBox').style.border = blackBorder;
-        document.getElementById('yellowBox').style.border = blackBorder;
-    } else if (box === 'sBox') { 
-        document.getElementById('mBox').style.border = blackBorder;
-        document.getElementById('lBox').style.border = blackBorder;
-    } else if (box === 'mBox') { 
-        document.getElementById('sBox').style.border = blackBorder;
-        document.getElementById('lBox').style.border = blackBorder;
-    } else if (box === 'lBox') { 
-        document.getElementById('sBox').style.border = blackBorder;
-        document.getElementById('mBox').style.border = blackBorder;
-    }
-}
-
-function chooseSizeIgnored (size) {
-    if (size === 'S') {
-        makeRedBorder('sBox');
-        returnBlackBorder('sBox');
-        
-    } else if (size === 'M') {
-        makeRedBorder('mBox');
-        returnBlackBorder('mBox');
-        
-    } else if (size === 'L') {
-        makeRedBorder('lBox');
-        returnBlackBorder('lBox');
+function activate(target, isActive) {
+    if (isActive === true) {
+        target.classList.add('product-details_active');
+        target.classList.remove('product-details_inactive');
+    } else {
+        target.classList.add('product-details_inactive');
+        target.classList.remove('product-details_active');
     }
 }
